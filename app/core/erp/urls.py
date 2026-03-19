8cf6b12bf8354fe5c724de08a5303724252afcd1
Emilio Rojas <emirodriguez624@gmail.com>
2026-03-19 14:41:47 -0500

    prueba de las urls

---
+++ app/core/erp/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+
+from core.erp.views import myfirstview
+
+urlpatterns = [
+    path('uno/', myfirstview),
+    path('dos/', myfirstview),
+]
+
--- app/core/erp/views.py
+++ app/core/erp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+
+# def myfirstview(request):
+#     return HttpResponse('Hola Mundo') 
+
+def myfirstview(request):
+    data = {
+        'name': 'Hades',
+        'lastname': 'Django',
+        'age': 1,
+    }
+    return JsonResponse(data)
